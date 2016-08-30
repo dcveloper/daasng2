c144dfe53d09731ddbd0489bce270f91660841cc
dcveloper <dcveloper@gmail.com>
2016-08-29 20:41:17 -0400

    Update project structure with content

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
-# TestRC5Proj
+# DaaS Project
 
 This project was generated with [angular-cli](https://github.com/angular/angular-cli) version 1.0.0-beta.11-webpack.2.
+
+How to run this project:
+
+1. Install Node/NPM.
+2. Run 'npm init' in the project directory. This will install the project dependencies.
+
+## Developer Notes
+
+1. Follow the official Angular.io style guide https://angular.io/styleguide
+2. When adding features, create unit tests (karma) and end to end tests.
+
 
 ## Development server
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
@@ -21,10 +32,6 @@ Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.
 
 Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/). 
 Before running the tests make sure you are serving the app via `ng serve`.
-
-## Deploying to Github Pages
-
-Run `ng github-pages:deploy` to deploy to Github Pages.
 
 ## Further help
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,44 @@ import { NgModule, ApplicationRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { OrganizationComponent } from './organization/organization.component';
+import { TopicComponent } from './topic/topic.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { MydashboardsComponent } from './mydashboards/mydashboards.component';
+import { AboutComponent } from './about/about.component';
+import { SubscribeComponent } from './subscribe/subscribe.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { ExplorerComponent } from './shared/explorer/explorer.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+
+import { routing, appRoutingProviders } from './app.routing';
+import { AutocompleteComponent } from './shared/explorer/autocomplete/autocomplete.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    OrganizationComponent,
+    TopicComponent,
+    DashboardComponent,
+    MydashboardsComponent,
+    AboutComponent,
+    SubscribeComponent,
+    NavbarComponent,
+    ExplorerComponent,
+    PagenotfoundComponent,
+    AutocompleteComponent
   ],
   imports: [
     BrowserModule,
     CommonModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   entryComponents: [AppComponent],
   bootstrap: [AppComponent]
 })
